ac8d75161bb8d9b4adf096cfb03aa8e73a0a2e66
michelle <foleymichelle9@gmail.com>
2020-03-18 13:12:56 -0600

    add initialize, get tests passing

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,5 +1,11 @@
 class Restaurant
 
-  def initialize(time, name) 
+  attr_reader :opening_time, :name, :dishes, :closing_time
+
+  def initialize(opening_time, name)
+    @opening_time = opening_time
+    @name = name
+    @dishes = []
+    @closing_time = closing_time
   end
 end
